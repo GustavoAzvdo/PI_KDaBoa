53128a02888d5524af0d19a9c83c0b6154310522
Gustavo Azevedo <ghca12@gmail.com>
2025-04-11 12:25:43 -0300

    subindo os arquivos

---
+++ kdaboa/src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar/Navbar'
+import './App.css'
+import { Grid,Box } from '@mui/material'
+function App() {
+
+  return (
+    <Box>
+        <Navbar />
+    </Box>
+  )
+}
+
+export default App
---
+++ kdaboa/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,50 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import logo from '../../assets/logo.png'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <>
+            <Box>
+                <AppBar position="static" className='navbar' elevation={0}>
+                    <Toolbar className='toolbar'>
+                        <Box className='btns-left'>
+                            <img src={logo} alt="" style={{ width: 50, height: 50 }} className='logo' />
+                            <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+                                <Button variant='text' color='inherit' size='large'>
+                                    <Typography>
+                                        Encontrar eventos
+                                    </Typography>
+                                </Button>
+                                <Button variant='text' color='inherit'>
+                                    <Typography >
+                                        Ajuda
+                                    </Typography>
+                                </Button>
+                            </Box>
+                        </Box>
+                        <Box className='btns-right' sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+                            <Button variant='text' color='inherit' size='large'>
+                                <Typography >
+                                    Entrar
+                                </Typography>
+                            </Button>
+                            <Button variant='contained' color='secondary' size='large' className='btnPublicar'>
+                                <Typography>
+                                    Publicar evento
+                                </Typography>
+                            </Button>
+                        </Box>
+                    </Toolbar>
+
+                </AppBar>
+            </Box>
+        </>
+    )
+}
+
+export default Navbar
