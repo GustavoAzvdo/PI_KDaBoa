3e6183f4f4f979b58b7b109150de53c826b4f308
Gustavo Azevedo <ghca12@gmail.com>
2025-05-26 12:16:16 -0300

    Arrumando a repsonsividade

--- kdaboa/src/pages/Home/Home.tsx
+++ kdaboa/src/pages/Home/Home.tsx
@@ -6,13 +6,19 @@ import BoxInfo from '../../components/BoxInfo/BoxInfo'
 import Title from '../../components/Title/Title'
 import Footer from '../../components/Footer/Footer'
 import hangloose from '../../assets/hangloose.png'
+import { Box } from '@mui/material'
 const Home = () => {
   return (
     <>
       <Navbar />
-      <Title>
-        Qual a boa de hoje? <img width="70" height="70" src={hangloose} alt="hang-ten" />
-      </Title>
+      <Box sx={{ 
+        textAlign: { xs: 'center' }, 
+        px: {xs: 2} }}>
+        <Title>
+          Qual a boa de hoje? <img width="70" height="70" src={hangloose} alt="hang-ten" />
+        </Title>
+
+      </Box>
       <Search />
       <Carrousel />
       <BoxInfo />
