0c0accac11016b7dbefcb87f24b75f4810da1390
Gustavo Azevedo <ghca12@gmail.com>
2025-06-17 21:51:53 -0300

    Arrumando o S do HTTP

--- kdaboa/src/api/api.ts
+++ kdaboa/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://apikdaboa-production.up.railway.app',
+    baseURL: 'https://apikdaboa-production.up.railway.app',
 });
 
 
