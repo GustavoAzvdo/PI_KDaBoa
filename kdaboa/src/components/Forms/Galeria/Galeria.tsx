4231fc46e727e914e63775ad3075e4017d2f2fdc
Gustavo Azevedo <ghca12@gmail.com>
2025-06-22 20:40:33 -0300

    Formatando imagens: tamanho , peso e formato

--- kdaboa/src/components/Forms/Galeria/Galeria.tsx
+++ kdaboa/src/components/Forms/Galeria/Galeria.tsx
@@ -1,14 +1,24 @@
-import { AttachFile, Close, CloudUpload, Delete } from '@mui/icons-material';
-import { Box, Button, Grid, IconButton, TextField, Typography } from '@mui/material'
+import { AttachFile, Check, Close, CloudUpload, Delete, InfoOutlined } from '@mui/icons-material';
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, IconButton, ListItem, ListItemIcon, ListItemText, TextField, Typography } from '@mui/material'
 import { styled } from '@mui/material/styles'
+import CustomSnackbar from '../../CustomSnackbar/CustomSnackbar';
 import React from 'react'
 
+
+const dicas = [
+    'Formatos permitidos: JPG, JPEG, PNG;',
+    'Tamanho máximo: 5MB;',
+    'Resolução: 1200 x 500.'
+]
 const Galeria = () => {
     const [fileName, setFileName] = React.useState<string>('');
     const [fileObj, setFileObj] = React.useState<File | null>(null);
     const [photos, setPhotos] = React.useState<{ name: string; url: string }[]>([]);
+    const [snackbarOpen, setSnackbarOpen] = React.useState(false);
+    const [snackbarMessage, setSnackbarMessage] = React.useState('');
+    const [snackbarSeverity, setSnackbarSeverity] = React.useState<'success' | 'warning'>('success');
+    const [open, setOpen] = React.useState(false);
     const inputRef = React.useRef<HTMLInputElement>(null);
-
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
@@ -20,21 +30,70 @@ const Galeria = () => {
     }
 
     const handleAddPhoto = () => {
-        if (fileObj && photos.length < 4) {
+        const tipoValido = ['image/jpeg', 'image/jpg', 'image/png'].includes(fileObj?.type || '');
+        if (!tipoValido) {
+            setSnackbarMessage('Formato inválido. Aceitos: JPG, JPEG, PNG.');
+            setSnackbarSeverity('warning');
+            setSnackbarOpen(true);
+            return;
+        }
+
+        if (fileObj?.size && fileObj.size > 5 * 1024 * 1024) { // 5MB
+            setSnackbarMessage('O tamanho máximo permitido é 5MB.');
+            setSnackbarSeverity('warning');
+            setSnackbarOpen(true);
+            return;
+        }
+
+        if (!fileObj) {
+            setSnackbarMessage('Nenhum arquivo selecionado.');
+            setSnackbarSeverity('warning');
+            setSnackbarOpen(true);
+            return;
+        }
+
+        if (photos.length >= 4) {
+            setSnackbarMessage('Você atingiu o limite máximo de 4 fotos.');
+            setSnackbarSeverity('warning');
+            setSnackbarOpen(true);
+            return;
+        }
+
+        // Validação final antes de adicionar
+        const img = new Image();
+        img.onload = function () {
+
+
             const url = URL.createObjectURL(fileObj);
             setPhotos([...photos, { name: fileObj.name, url }]);
+            setSnackbarMessage('Foto adicionada com sucesso!');
+            setSnackbarSeverity('success');
+            setSnackbarOpen(true);
             setFileName('');
             setFileObj(null);
             if (inputRef.current) {
                 inputRef.current.value = '';
             }
-        }
+        };
+
+        img.onerror = function () {
+            setSnackbarMessage('Erro ao processar a imagem. Tente novamente.');
+            setSnackbarSeverity('warning');
+            setSnackbarOpen(true);
+        };
+
+        img.src = URL.createObjectURL(fileObj);
     }
 
     const handleRemovePhoto = (index: number) => {
         setPhotos((prev) => {
             URL.revokeObjectURL(prev[index].url);
+            URL.revokeObjectURL(prev[index].url);
+            setSnackbarMessage('Foto removida com sucesso!');
+            setSnackbarSeverity('warning');
+            setSnackbarOpen(true);
             return prev.filter((_, i) => i !== index);
+
         })
     }
     const VisuallyHiddenInput = styled('input')({
@@ -51,8 +110,10 @@ const Galeria = () => {
 
     return (
         <Grid container spacing={2} sx={{ padding: 2 }}>
+            {/* Campo de texto q vai receber o nome da foto */}
             <Grid size={{ xs: 12, sm: 6, md: 3 }}>
                 <TextField
+
                     disabled
                     sx={{
                         '& .MuiOutlinedInput-root': {
@@ -67,6 +128,7 @@ const Galeria = () => {
                     variant="outlined"
                     fullWidth
                     inputProps={{ readOnly: true }}
+                    InputLabelProps={{ shrink: true }}
                     InputProps={{
                         endAdornment: fileName && (
                             <>
@@ -81,6 +143,7 @@ const Galeria = () => {
                     }}
                 />
             </Grid>
+            {/* Botão de adicionar foto */}
             <Grid size={{ xs: 12, sm: 6, md: 3 }}>
                 <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', pt: 1 }}>
                     <Button
@@ -113,6 +176,17 @@ const Galeria = () => {
                     </Button>
                 </Box>
             </Grid>
+
+            {/* Dicas de como adicionar uma boa foto */}
+            <Grid size={{ xs: 12, sm: 6, md: 1 }}>
+                <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', mt: 1 }}>
+                    <IconButton color='primary' onClick={() => setOpen(true)}>
+                        <InfoOutlined />
+                    </IconButton>
+
+                </Box>
+            </Grid>
+
             <Grid size={{ xs: 12, sm: 6, md: 12 }} sx={{ borderBottom: '1px solid #eee' }}>
                 <Box sx={{ width: '100%', display: 'flex', justifyContent: 'flex-end', alignItems: 'center', py: 2 }}>
                     <Button
@@ -121,7 +195,7 @@ const Galeria = () => {
                         sx={{
                             backgroundColor: 'var(--roxo)',
                             px: 6,
-                           
+
                         }}
                         onClick={handleAddPhoto}
                         disabled={!fileObj || photos.length >= 4}
@@ -158,6 +232,36 @@ const Galeria = () => {
                     </Box>
                 </Grid>
             ))}
+            <CustomSnackbar
+                open={snackbarOpen}
+                message={snackbarMessage}
+                severity={snackbarSeverity}
+                onClose={() => setSnackbarOpen(false)}
+            />
+            <Dialog open={open} onClose={() => setOpen(false)}>
+                <DialogTitle sx={{ fontSize: 22, fontFamily: 'var(--notosans) !important' }}>Dicas de como adicionar uma boa foto</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', pt: 1 }}>
+                            <Box>
+                                {
+                                    dicas.map((value, idx) => (
+                                        <ListItem key={idx} sx={{ p: 0 }}>
+                                            <ListItemIcon sx={{ p: 0 }}>
+                                                <Check color='primary' />
+                                            </ListItemIcon>
+                                            <ListItemText primary={value} sx={{ p: 0 }} />
+                                        </ListItem>
+                                    ))
+                                }
+                            </Box>
+                        </Box>
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setOpen(false)} variant="contained" color="primary" sx={{ fontSize: 16 }}>Fechar</Button>
+                </DialogActions>
+            </Dialog>
         </Grid>
     )
 }
