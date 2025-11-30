c6432d28247ff0fac3e9101d5e5038ee41192099
Gustavo Azevedo <ghca12@gmail.com>
2025-11-29 21:33:24 -0300

    fix: update API base URL to use the correct development endpoint

--- kdaboa/src/api/api.ts
+++ kdaboa/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://vmlxc5dd-3000.brs.devtunnels.ms',
     withCredentials: true,
 });
 
